e847b46f3e9906d00176740e807ae5fa1f0684f5
Tyrone Williams <itenory@gmail.com>
2019-04-15 03:13:28 -0400

    Add routes for game and 404 page

--- app/src/components/Root.js
+++ app/src/components/Root.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
 import HomePage from './home/HomePage';
+import GamePage from './game/GamePage';
+import NotFoundPage from './NotFoundPage';
 
 const Root = () => (
   <Router>
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-    </Switch>
+    <MainLayout>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/game/:roomId" component={GamePage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </MainLayout>
   </Router>
 );
 
