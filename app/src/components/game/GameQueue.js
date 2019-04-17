4c973d25a44e1cc1b43f6c3ff329f7f012a80ddd
Tyrone Williams <itenory@gmail.com>
2019-04-17 18:56:27 -0400

    Now displays a game room link to share

--- app/src/components/game/GameQueue.js
+++ app/src/components/game/GameQueue.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const GameQueue = props => (
-  <section className="">Waiting for players to join/ready</section>
+  <section className="">
+    <div className="">
+      <p>Link to Join Game: </p>
+      <span>
+        {window.location.host}
+        /game/
+        {props.roomId}
+      </span>
+    </div>
+  </section>
 );
+
+GameQueue.propTypes = {
+  roomId: PropTypes.string.isRequired
+};
 
 export default GameQueue;
