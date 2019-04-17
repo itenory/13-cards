a2835d2151faf80535aea6f2c686ae119f5c11b5
Tyrone Williams <itenory@gmail.com>
2019-04-17 18:10:08 -0400

    Add feature to create game room

--- app/src/components/home/HomePage.js
+++ app/src/components/home/HomePage.js
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { ajaxRequest } from '../../utils/utils';
+import Notification from '../shared/Notification';
 
-const HomePage = () => <div>The homepage</div>;
+class HomePage extends Component {
+  state = {
+    roomId: null,
+    matchError: null
+  };
+
+  createRoom = () => {
+    ajaxRequest('/create/room', 'post')
+      .then(res => {
+        // If a room id is sent back, redirect to room page
+        if (res.data.roomId) {
+          this.setState({ roomId: res.data.roomId });
+        }
+      })
+      .catch(err => {
+        this.setState({ matchError: err.response.data });
+      });
+  };
+
+  render() {
+    const { roomId, matchError } = this.state;
+    if (roomId) return <Redirect to={`/game/${roomId}`} />;
+
+    return (
+      <section className="">
+        {matchError && (
+          <Notification
+            message={matchError}
+            clearError={() => this.setState({ matchError: null })}
+          />
+        )}
+        <button type="button" onClick={this.createRoom}>
+          Create room
+        </button>
+      </section>
+    );
+  }
+}
 
 export default HomePage;
