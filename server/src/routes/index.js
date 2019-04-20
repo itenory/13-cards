98083736cfd1f355a2316b1373fb3cec609496cd
Tyrone Williams <itenory@gmail.com>
2019-04-19 21:19:57 -0400

    Created routes for getting and creating rooms

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,5 +1,25 @@
+const {
+  createRoomRoute,
+  getRoomData
+} = require('../controllers/roomController');
+
 /**
  * Sets app to use routers.
  * @param {Object} app Express app
  */
-module.exports = function setUpRouters(app) {};
+module.exports = function setUpRouters(app) {
+  app.post('/create/room', createRoomRoute);
+
+  app.get('/game/:roomId/data', getRoomData);
+
+  // Error handler
+  app.use((err, req, res, next) => {
+    if (err) {
+      return res
+        .status(err.status || 500)
+        .send(err.msg || 'Server error. Please try again');
+    }
+
+    res.status(500).send('Server error. Please try again.');
+  });
+};
