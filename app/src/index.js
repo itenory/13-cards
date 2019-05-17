479d3a250fa651102a35a8df28220a06796d8896
williamstyronejr <itenory@gmail.com>
2019-05-17 17:47:11 -0400

    Connect to socket on launch

--- app/src/index.js
+++ app/src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Root from './components/Root';
+import { setupSocket } from './utils/socket';
 
+setupSocket('localhost:5000');
 ReactDOM.render(<Root />, document.getElementById('root'));
