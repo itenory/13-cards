f6b1d0768da3ef71fa49fa5cf8ad0edf502766b2
williamstyronejr <itenory@gmail.com>
2019-05-17 17:43:04 -0400

    Created nav bar

--- app/src/components/layouts/Header.js
+++ app/src/components/layouts/Header.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import './styles/header.css';
 
-const Header = () => <header className="">page header</header>;
+const Header = () => (
+  <header className="page-header">
+    <nav className="header__nav">
+      <ul className="header__list">
+        <li className="header__item">
+          <a className="header__link" href="/">
+            Home
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
 
 export default Header;
