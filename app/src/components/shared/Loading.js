294d53a45873b31f09cc3cf1ea9238b26864736a
williamstyronejr <itenory@gmail.com>
2019-05-17 17:39:54 -0400

    Add message element

--- app/src/components/shared/Loading.js
+++ app/src/components/shared/Loading.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Loading = ({ error }) => (
+const Loading = ({ message, error }) => (
   <div className="loading">
     Loading ...
+    <span className="loading__message">{message}</span>
     <span className="loading__error">{error}</span>
   </div>
 );
 
 Loading.propTypes = {
+  message: PropTypes.string,
   error: PropTypes.string
 };
 
 Loading.defaultProps = {
+  message: null,
   error: null
 };
 
