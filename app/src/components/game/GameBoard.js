b5aa613ef04f610d3205d4e54be18fa7b5a8a275
williamstyronejr <itenory@gmail.com>
2019-05-17 17:41:15 -0400

    Updated layout for game board with mobile view

--- app/src/components/game/GameBoard.js
+++ app/src/components/game/GameBoard.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import {} from '../../utils/socket';
+import './styles/gameboard.css';
 
 class GameBoard extends Component {
   state = {};
 
   render() {
     return (
-      <section>
-        Game board
-        <div />
+      <section className="game">
+        <div className="game__top">
+          <div className="game__info">info</div>
+          <div className="game__player game__player-top">p2</div>
+        </div>
+
+        <div className="game__middle">
+          <div className="game__player game__player-left">p1</div>
+
+          <div className="game__board">game board</div>
+          <div className="game__player game__player-right">p3</div>
+        </div>
+
+        <div className="game__bottom">
+          <div className="game__player game__player-bottom">p4</div>
+        </div>
       </section>
     );
   }
