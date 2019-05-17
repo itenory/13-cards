568774df16732a8405397a12802119530cb5af1c
williamstyronejr <itenory@gmail.com>
2019-05-17 17:42:16 -0400

    Updated layout

--- app/src/components/game/GameQueue.js
+++ app/src/components/game/GameQueue.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Loading from '../shared/Loading';
+import { startGame } from '../../utils/socket';
+import './styles/gamequeue.css';
 
-const GameQueue = props => (
-  <section className="">
-    <div className="">
-      <p>Link to Join Game: </p>
-      <span>
-        {window.location.host}
-        /game/
-        {props.roomId}
-      </span>
-    </div>
-  </section>
-);
+class GameQueue extends Component {
+  componentDidMount() {
+    // Add listeners for queue
+  }
+
+  componentWillUnmount() {
+    // Remove listeners for queue
+  }
+
+  render() {
+    const { roomId, playerCount } = this.props;
+
+    return (
+      <section className="queue">
+        <Loading message={`${playerCount}/4 players joined`} />
+        <div className="queue__options">
+          <p className="queue__invite">
+            {`${window.location.host}/game/${roomId}`}
+          </p>
+
+          <button
+            type="button"
+            className="btn btn-med btn-start"
+            onClick={startGame}
+          >
+            Start Game
+          </button>
+        </div>
+      </section>
+    );
+  }
+}
 
 GameQueue.propTypes = {
   roomId: PropTypes.string.isRequired
