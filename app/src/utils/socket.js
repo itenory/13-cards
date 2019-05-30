4ad8c0f5851f8c395dfcb6ee5d0bd35b27f0e778
williamstyronejr <itenory@gmail.com>
2019-05-30 19:41:10 -0400

    Created handlers for game queue

--- app/src/utils/socket.js
+++ app/src/utils/socket.js
@@ -1,14 +1,64 @@
 import io from 'socket.io-client';
 
 let socket;
+const QUEUEUPDATE = 'queueUpdate';
+const NEWPLAYER = 'newPlayer';
 
 export function setupSocket(host, options = {}) {
-  io.connect(host, options);
+  socket = io.connect(host, options);
+}
+
+/**
+ *
+ * @param {Function} cb Event handler for game queue events.
+ */
+export function subscribeQueue(cb) {
+  socket.on(QUEUEUPDATE, cb);
+}
+
+/**
+ *
+ */
+export function unsubscribeQueue() {
+  socket.off(QUEUEUPDATE);
+  socket.off(NEWPLAYER);
 }
 
 /**
  * Emits a event to server to start game.
  */
-export function startGame() {
-  socket.emit('startGame');
+export function startGame(gameId) {
+  socket.emit('startGame', gameId);
+}
+
+/**
+ *
+ * @param {Array<String>} cards
+ */
+export function playCards(cards, player) {
+  socket.emit('playCards', cards, player);
+}
+
+/**
+ *
+ * @param {String} player
+ */
+export function passTurn(player) {
+  socket.emit('passTurn', player);
+}
+
+/**
+ * Emits a socket event for client to join game room.
+ * @param {String} gameId Id of game to join
+ */
+export function joinGame(gameId) {
+  socket.emit('joinGame', gameId);
+}
+
+/**
+ * Emits a socket event for client to leave game room.
+ * @param {String} gameId Id of game to leave
+ */
+export function leaveGame(gameId) {
+  socket.emit('leaveGame', gameId);
 }
