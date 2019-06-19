7e32f8015e21426ca895a9ef0e7e8f40f14f1547
williamstyronejr <itenory@gmail.com>
2019-06-19 01:02:09 -0400

    Component now acts a source of truth for game

--- app/src/components/game/GamePage.js
+++ app/src/components/game/GamePage.js
@@ -3,12 +3,22 @@ import { Redirect } from 'react-router-dom';
 import { ajaxRequest } from '../../utils/utils';
 import GameQueue from './GameQueue';
 import GameBoard from './GameBoard';
+import Loading from '../shared/Loading';
+import {
+  startGame,
+  subscribeQueue,
+  unsubscribeQueue,
+  joinGame,
+  getUserId
+} from '../../utils/socket';
 
 class GamePage extends Component {
   state = {
     roomId: this.props.match.params.roomId,
     matchStart: false,
-    matchError: null
+    matchError: null,
+    matchLoading: true,
+    playerCount: 0
   };
 
   /**
@@ -20,24 +30,68 @@ class GamePage extends Component {
     if (roomId) {
       ajaxRequest(`/game/${roomId}/data`, 'get')
         .then(res => {
+          if (!res.data.started) {
+            return this.setState({
+              matchStart: false,
+              matchLoading: false
+            });
+          }
+
+          if (res.data.players && res.data.players.indexOf(getUserId()) >= 0) {
+            return this.setState({
+              matchStart: true,
+              matchLoading: false
+            });
+          }
+
           this.setState({
-            matchStart: res.data.ready
+            matchError: 'Not correct user',
+            matchLoading: false
           });
         })
         .catch(err => {
+          console.log(err);
           this.setState({
-            matchError: err.response.data
+            matchError: err.response.data,
+            matchLoading: false
           });
         });
     }
   }
 
+  /**
+   * Updates game page state
+   * @param {Object} state
+   */
+  updateGameState = state => {
+    this.setState({ ...state });
+  };
+
   render() {
-    const { roomId, matchStart, matchError } = this.state;
+    const {
+      roomId,
+      matchStart,
+      matchError,
+      matchLoading,
+      playerCount
+    } = this.state;
+
+    if (matchLoading) return <Loading message="Grabbing game data" />;
 
     if (!roomId || matchError) return <Redirect to="/" />;
 
-    if (!matchStart) return <GameQueue playerCount={1} roomId={roomId} />;
+    if (!matchStart)
+      return (
+        <GameQueue
+          roomId={roomId}
+          playerCount={playerCount}
+          updateGameState={this.updateGameState}
+          joinGame={joinGame}
+          startGame={startGame}
+          subscribeQueue={subscribeQueue}
+          unsubscribeQueue={unsubscribeQueue}
+        />
+      );
 
     return <GameBoard roomId={roomId} />;
   }
