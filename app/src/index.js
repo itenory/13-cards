b143a0670de0e73559a1028b8cd62b63e72d37d0
williamstyronejr <itenory@gmail.com>
2019-06-19 00:16:05 -0400

    Added reconnection attempt limit

--- app/src/index.js
+++ app/src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import Root from './components/Root';
 import { setupSocket } from './utils/socket';
 
-setupSocket('localhost:5000');
+setupSocket('localhost:5000', {
+  reconnectionAttempts: 10
+});
 ReactDOM.render(<Root />, document.getElementById('root'));
